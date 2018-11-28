f1c9cce36a800e50a4a10e04c9cf2221d7ccd3af
mmchost <41358529+mmchost@users.noreply.github.com>
2018-11-28 12:04:24 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function Page_OnResize()
 {
+	//data:image/png;base64,
     var banner1920 = "<img style=\"border-style:none; max-width:1920px; max-height:480px; width:100%;\" src=\"images/banner/1920x480.png\" alt=\"radiografii dentare, tomografii 3d cbct, radiologie dentara\"/>";
 	var banner1280 = "<img style=\"border-style:none; max-width:1280px; max-height:480px; width:100%;\" src=\"images/banner/1280x480.png\" alt=\"radiografii dentare, tomografii 3d cbct, radiologie dentara\"/>";
 	var banner960 = "<img style=\"border-style:none; max-width:960px; max-height:480px; width:100%;\" src=\"images/banner/960x480.png\" alt=\"radiografii dentare, tomografii 3d cbct, radiologie dentara\"/>";
@@ -20,44 +21,135 @@ function Page_OnResize()
 	var mapID = "map";
     var w = window.innerWidth || document.documentElement.clientWidth || documentBody.clientWidth;    
 	
+	var banner = document.getElementById(bannerID);
+	var map = document.getElementById(mapID)
+		
     if (w>=1280)
 	{
-		document.getElementById(bannerID).innerHTML = banner1920;
-		document.getElementById(mapID).innerHTML = map1920;
+		if (banner)
+		{
+			banner.innerHTML = banner1920;
+		}
+		if (map)
+		{		
+			map.innerHTML = map1920;
+		}
 	}
     if (w<=1280)
     {
-		document.getElementById(bannerID).innerHTML = banner1280;
-		document.getElementById(mapID).innerHTML = map1280;
+		if (banner)
+		{
+			banner.innerHTML = banner1280;
+		}
+		if (map)
+		{
+			map.innerHTML = map1280;
+		}
     }
     if (w<=960)
     {
-		document.getElementById(bannerID).innerHTML = banner960;
-		document.getElementById(mapID).innerHTML = map960;
+		if (banner)
+		{
+			banner.innerHTML = banner960;
+		}
+		if (map)
+		{
+			map.innerHTML = map960;
+		}
     }
 	if (w<=640)
     {
-		document.getElementById(bannerID).innerHTML = banner640;
-		document.getElementById(mapID).innerHTML = map640;
+		if (banner)
+		{
+			banner.innerHTML = banner640;
+		}
+		if (map)
+		{
+			map.innerHTML = map640;
+		}
     }
 	if (w<=480)
     {
-		document.getElementById(bannerID).innerHTML = banner480;
-		document.getElementById(mapID).innerHTML = map480;
+		if (banner)
+		{
+			banner.innerHTML = banner480;
+		}
+		if (map)
+		{
+			map.innerHTML = map480;
+		}
     }
 	if (w<=320)
-    {
-		document.getElementById(bannerID).innerHTML = banner320;
-		document.getElementById(mapID).innerHTML = map320;
+    {	
+		if (banner)
+		{
+			banner.innerHTML = banner320;
+		}
+		if (map)
+		{
+			map.innerHTML = map320;
+		}
     }
 	if (w<=240)
     {
-		document.getElementById(bannerID).innerHTML = banner240;
-		document.getElementById(mapID).innerHTML = map240;
+		if (banner)
+		{
+			banner.innerHTML = banner240;
+		}
+		if (map)
+		{
+			map.innerHTML = map240;
+		}
     }
 }
 
 function Page_OnLoad() 
 {
+	var wb = document.getElementById("warning-bar");
+	var sectionTag1 = document.getElementById("sectionTag1");
+	var sectionTag2 = document.getElementById("sectionTag2");
+	var sectionTag3 = document.getElementById("sectionTag3");
+	if (wb)
+	{
+		var mainContent = document.getElementById("main-content");
+		var header = document.getElementById("header");
+		if (header)
+		{
+			header.setAttribute('style', 'top:48px;');
+		}
+		if (mainContent)
+		{
+			mainContent.setAttribute('style', 'top:128px;');
+		}
+	
+		if (sectionTag1)
+		{
+			sectionTag1.setAttribute('style', 'height:128px; margin-top:-128px;');
+		}
+		if (sectionTag2)
+		{
+			sectionTag2.setAttribute('style', 'height:128px; margin-top:-128px;');
+		}
+		if (sectionTag3)
+		{
+			sectionTag3.setAttribute('style', 'height:128px; margin-top:-128px;');
+		}
+	}
+	else	
+	{
+		if (sectionTag1)
+		{
+			sectionTag1.setAttribute('style', 'height:80px; margin-top:-80px;');
+		}
+		if (sectionTag2)
+		{
+			sectionTag2.setAttribute('style', 'height:80px; margin-top:-80px;');
+		}
+		if (sectionTag3)
+		{
+			sectionTag3.setAttribute('style', 'height:80px; margin-top:-80px;');
+		}
+	}
+
 	Page_OnResize();
 }
